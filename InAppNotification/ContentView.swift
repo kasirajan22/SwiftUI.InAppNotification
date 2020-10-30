810f0bcc34ba676cb0202c2d151044c75f0b6e2d
kasirajan22 <kasi.srm@gmail.com>
2020-10-30 06:33:18 +0530

    Initial Commit

---
+++ InAppNotification/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InAppNotification
+//
+//  Created by Magizhan on 30/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InAppNotification/InAppNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  InAppNotificationApp.swift
+//  InAppNotification
+//
+//  Created by Magizhan on 30/10/20.
+//
+
+import SwiftUI
+
+@main
+struct InAppNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
